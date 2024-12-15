f4b3e0f389ec558f974b0e7b8e62f7d40027527a
evadev6 <ed240@st-andrews.ac.uk>
2024-12-15 18:28:03 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get the button element by its ID
+const button = document.getElementById('myButton');
+
+// Add an event listener to the button
+button.addEventListener('click', function() {
+    alert('You clicked the button!');
+});
